789f3f8f905ee419f4aa471599e4ab5b1ca51094
Mohammed Jasam <mnqnd@mst.edu>
2017-04-12 17:54:08 -0500

    Implementing the model in python

---
+++ Python Script/main.py
@@ -0,0 +1,6 @@
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+
+
+CLARIFAI_APP_ID = '21IgMMOfoj5kD4gAOUtKjJN3sNWbwpkCxpzwRH3b'
+CLARIFAI_APP_SECRET = 'JHFF2_adodG9-Av7AJEk4vyBKnPu3e-rZ_S0cTh0'
