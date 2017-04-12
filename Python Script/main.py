22adb96cbb1623115bc20e8bafd2a73297fa6356
Mohammed Jasam <mnqnd@mst.edu>
2017-04-12 18:29:02 -0500

    Generated the model

--- Python Script/main.py
+++ Python Script/main.py
@@ -9,19 +9,19 @@ CLARIFAI_APP_SECRET = 'YvaCBTKd4dcuNFE3pJlp4wRELtbpugfDKN6s4YnI'
 app = ClarifaiApp(CLARIFAI_APP_ID, CLARIFAI_APP_SECRET)
 
 
-# import a few labeld images
-app.inputs.create_image_from_url(url="https://samples.clarifai.com/dog1.jpeg", concepts=["cute dog"], not_concepts=["cute cat"])
-app.inputs.create_image_from_url(url="https://samples.clarifai.com/dog2.jpeg", concepts=["cute dog"], not_concepts=["cute cat"])
-
-app.inputs.create_image_from_url(url="https://samples.clarifai.com/cat1.jpeg", concepts=["cute cat"], not_concepts=["cute dog"])
-app.inputs.create_image_from_url(url="https://samples.clarifai.com/cat2.jpeg", concepts=["cute cat"], not_concepts=["cute dog"])
-
-model = app.models.create(model_id="pets", concepts=["cute cat", "cute dog"])
-
-model = model.train()
-
-# predict with samples
-a= model.predict_by_url(url="https://samples.clarifai.com/dog3.jpeg")
-print a
-print 'HIiiiiiiiiiiiiiiiiiiiiiiiii'
-# print model.predict_by_url(url="https://samples.clarifai.com/cat3.jpeg")
+# # import a few labeld images
+# app.inputs.create_image_from_url(url="https://samples.clarifai.com/dog1.jpeg", concepts=["cute dog"], not_concepts=["cute cat"])
+# app.inputs.create_image_from_url(url="https://samples.clarifai.com/dog2.jpeg", concepts=["cute dog"], not_concepts=["cute cat"])
+#
+# app.inputs.create_image_from_url(url="https://samples.clarifai.com/cat1.jpeg", concepts=["cute cat"], not_concepts=["cute dog"])
+# app.inputs.create_image_from_url(url="https://samples.clarifai.com/cat2.jpeg", concepts=["cute cat"], not_concepts=["cute dog"])
+#
+# model = app.models.create(model_id="pets", concepts=["cute cat", "cute dog"])
+#
+# model = model.train()
+#
+# # predict with samples
+# a= model.predict_by_url(url="https://samples.clarifai.com/dog3.jpeg")
+# print a
+# print 'HIiiiiiiiiiiiiiiiiiiiiiiiii'
+# # print model.predict_by_url(url="https://samples.clarifai.com/cat3.jpeg")
