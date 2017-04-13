a2cdeeeda3986566ba437b93ce13337f57c07195
Mohammed Jasam <mnqnd@mst.edu>
2017-04-12 19:46:09 -0500

    working on dynamic prediction

--- Python Script/main.py
+++ Python Script/main.py
@@ -1,14 +1,22 @@
 from __future__ import print_function
 import json
 from clarifai.rest import ClarifaiApp
+import subprocess
+import sys
+from clarifai.rest import Image as ClImage
 
 app = ClarifaiApp("21IgMMOfoj5kD4gAOUtKjJN3sNWbwpkCxpzwRH3b", "JHFF2_adodG9-Av7AJEk4vyBKnPu3e-rZ_S0cTh0")
 
 model = app.models.get('ProfileGen')
 
-s= model.predict_by_url(url='https://8296-presscdn-0-77-pagely.netdna-ssl.com/wp-content/uploads/images/headline/140117_cigarettes.jpg')
+
+image = ClImage(file_obj=open(sys.argv, 'rb'))
+s= model.predict(image)
+
 
 ss = json.dumps(s, indent=4, sort_keys=True);
 
 with open('op.json', 'w') as f:
     print(ss, file=f)
+f.close()
+subprocess.call('python print.py',shell=true)
