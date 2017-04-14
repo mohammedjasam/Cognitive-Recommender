586d848bbe6d27f55a8266a72a427d4cb1be1c5d
Mohammed Jasam <mnqnd@mst.edu>
2017-04-14 17:38:59 -0500

    Training the model with precision!

--- Python Script/main.py
+++ Python Script/main.py
@@ -8,7 +8,9 @@ from subprocess import Popen, PIPE
 import os
 
 
-app = ClarifaiApp("21IgMMOfoj5kD4gAOUtKjJN3sNWbwpkCxpzwRH3b", "JHFF2_adodG9-Av7AJEk4vyBKnPu3e-rZ_S0cTh0")
+# app = ClarifaiApp("21IgMMOfoj5kD4gAOUtKjJN3sNWbwpkCxpzwRH3b", "JHFF2_adodG9-Av7AJEk4vyBKnPu3e-rZ_S0cTh0")  #Version 1 mnqnd
+app = ClarifaiApp("b9Eaq-zthkRQ6_5N0LpyXGfMkgnt4qLRr0KOILHC", "mh3-rEVbYqVaQ2cKHMRUECtClG572XRGf2Emcp1v") #Version 2 mdjasam1171
+
 model = app.models.get('ProfileGen')
 del sys.argv[0]
 imageUrl = sys.argv[0]
