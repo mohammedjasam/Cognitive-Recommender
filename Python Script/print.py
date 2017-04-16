6c326f7c664ef5964a388007684bc7bae7275994
Mohammed Jasam <mnqnd@mst.edu>
2017-04-16 03:08:26 -0500

    Converted the data to match the existing data

--- Python Script/print.py
+++ Python Script/print.py
@@ -22,7 +22,76 @@ db = firebase.database()
 data = dict(data)
 ll = data['outputs'][0]['data']['concepts']
 
+# print(ll)
 
+d={}
 for x in ll:
-    print(x['name'],str(x['value']*100)+"%")
-    db.child('User').child('1').push('{'+x['name']+':'+str(x['value']*100)+"%"+'}')
+    d[x['name']]=x['value']*100
+    # print(x['name'],str(x['value']*100)+"%")
+    # db.child('User').child('1').push('{'+x['name']+':'+str(x['value']*100)+"%"+'}')
+print(d)
+finalList = {}
+
+
+###### Smoker
+if d['smoker']<5:
+    finalList['smoker']=0
+else:
+    finalList['smoker']=1
+
+###### Drinker
+if d['drinker']<5:
+    finalList['drink_level']=0
+else:
+    finalList['drink_level']=1
+
+###### Informal Dress
+if d['informal']<d['formal']:
+    finalList['dress_preference']=1
+else:
+    finalList['dress_preference']=0
+
+###### Family
+if d['family']>d['friends'] and d['family']>d['single']:
+    finalList['ambience']=0
+elif d['friends']>d['family'] and d['friends']>d['single']:
+    finalList['ambience']=1
+else:
+    finalList['ambience']=2
+
+###### Student
+if d['student']>d['professional']:
+    finalList['activity']=0
+else:
+    finalList['activity']=1
+# print(finalList)
+
+if finalList['dress_preference']==1:
+    finalList['budget']=1
+else:
+    finalList['budget']=0
+# print(finalList)
+#### is a student and single and formal ==> med
+if finalList['activity']==0 and finalList['ambience']==2 and finalList['dress_preference']==1:
+    finalList['budget']=1
+#### is a student and single and informal ==> low
+elif finalList['activity']==0 and finalList['ambience']==2 and finalList['dress_preference']==0:
+    finalList['budget']=0
+#### is a student and friends and formal ==> high
+elif finalList['activity']==0 and finalList['ambience']==1 and finalList['dress_preference']==1:
+    finalList['budget']=2
+#### is a student and friends and informal ==> med
+elif finalList['activity']==0 and finalList['ambience']==1 and finalList['dress_preference']==0:
+    finalList['budget']=1
+#### is a professional and single and informal ==> low
+elif finalList['activity']==1 and finalList['ambience']==2 and finalList['dress_preference']==0:
+    finalList['budget']=0
+#### is a professional and friends and informal ==> med
+elif finalList['activity']==1 and finalList['ambience']==1 and finalList['dress_preference']==0:
+    finalList['budget']=1
+#### is a professional and friends and formal ==> high
+elif finalList['activity']==1 and finalList['ambience']==2 and finalList['dress_preference']==0:
+    finalList['budget']=2
+
+print(finalList)
+# print(finalList)
