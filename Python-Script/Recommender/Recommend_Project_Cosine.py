b36c6104b5df1e2d54d185a3ec4a8de6aa692e40
Mohammed Jasam <mnqnd@mst.edu>
2017-04-17 03:15:31 -0500

    Running the Whole Kit! Works fine as long as there are no duplicates! Need to resolve that issue!
    TODO: Remove Duplicate timestamps to the users!

--- Python-Script/Recommender/Recommend_Project_Cosine.py
+++ Python-Script/Recommender/Recommend_Project_Cosine.py
@@ -78,16 +78,15 @@ Users=[]
 Restraunts = []
 UserRes={}
 
-
-
-
-for i in range(5):
+noOfUsers=0
+for i in range(10):
     d=distance_frame.iloc[i+1]["idx"]
     u=User_Profile.loc[int(d)]['userID']
     r=user_restaurant_final.loc[user_restaurant_final['userID'] == u]
     Users.append(u)
     Restraunts+=list(r['name'])
     UserRes[u]=list(r['name'])
+    noOfUsers+=1
 
 
 print("\n")
@@ -95,39 +94,26 @@ print("===================================")
 print("||The User Being tested is: "+ get_lastUID("Testing_Binary.csv")+"||")
 print("===================================")
 print()
-print("The following are the top 5 Users similar to the user in the picture and their corresponding restaurant choices!")
+print("The following are the top %s Users similar to the user in the picture and their corresponding restaurant choices!" %noOfUsers)
 print("================================================================================================================\n")
-# print(Users)
-# print()
-# print(Restraunts)
-# print("\n")
-# print()
-# print(len(Restraunts))
-# print()
-# print(len(set(Restraunts)))
+# print(UserRes)
+for k,v in UserRes.items():
+    print(k,v)
+print("_____________________________________________________________________________________________________________________________________________________________________________________________________________")
+print()
+old=Restraunts
+new=list(set(Restraunts))
+
+
+for x in new:
+    old.remove(x)
+print("The Similar Restraunts among the Similar Users!")
+print("===============================================\n")
+common = old
+for x in common:
+    print(x)
+# print(len(common))
 #
 import json
-
-# # load from file:
-# with open('/path/to/my_file.json', 'r') as f:
-#     try:
-#         data = json.load(f)
-#     # if the file is empty the ValueError will be thrown
-#     except ValueError:
-#         data = {}
-
-# save to file:
-sss=json.dumps(UserRes)#, indent=4, sort_keys=True)
-# print(sss)
 with open('Restraunts.json', 'w') as f:
     json.dump(UserRes,f,indent=4,ensure_ascii=False)
-    # print(sss,f)
-#     # data['new_key'] = [1, 2, 3]
-#     sss=json.dump(UserRes,indent=4, sort_keys=True)
-#
-#     json.dump(UserRes,indent=4, sort_keys=True, f)
-# #
-# with open('Restraunts.csv','w') as f:
-#     # for k,v in UserRes.items():
-#     #     print((k,v),f)
-#     print(Restraunts,f)
