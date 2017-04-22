94193dafc0eeadb3d3f52776382cde8bd71608a1
Mohammed Jasam <mnqnd@mst.edu>
2017-04-21 22:51:09 -0500

    Working on dynamic Restaurant generation

---
+++ Python-Script/Foursquare/RestaurantExtractor.py
@@ -0,0 +1,51 @@
+import json
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path+'\\DataSets\\')
+# key=['Restaurant','chicken','steak','pizza','veg']
+key=['categories']
+l=[]
+names=[]
+
+for x in key:
+    with open(x+'.json') as data_file:
+        data = json.load(data_file)
+
+    # ll=[]
+    data = dict(data)
+
+    for i in range(100):
+        try:
+            x= data['venues'][i]['name']
+            names.append(data['venues'][i]['name'])
+            print(x)
+        except:
+            pass
+# for i in range(30):
+#     try:
+#         x=data['venues'][i]['hasMenu']
+#         l.append(i)
+#         ll.append(x)
+#     except:
+#         pass
+#     try:
+#         y=data['venues'][i]['allowMenuUrlEdit']
+#         l.append(i)
+#         ll.append(x)
+#     except:
+#         pass
+#
+#     print(x)
+# print(list(set(l)))
+
+
+#
+# data = dict(data)
+# ll = data['outputs'][0]['data']['concepts']
+#
+# # print(ll)
+#
+# d={}
+# for x in ll:
+#     d[x['name']]=x['value']*100
+#     print(x['name'],str(x['value']*100)+"%")
---
+++ Python-Script/Foursquare/localRecommender.py
@@ -0,0 +1,19 @@
+import foursquare
+from foursquare import Foursquare
+import json
+
+client = foursquare.Foursquare(client_id='Q3JHJJ1TS1XOR0TK1LTF5OFJHQC4OTLHMBIK1GYOIYXORSMB', client_secret='C54VXQIUTU2PKCPZI3RJQICLYNLYKZQ40ST1T50PYN5YVHUO')
+key=['Restaurant','chicken','steak','pizza','veg']
+
+client.users('9650322')
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path+'\\DataSets\\')
+for x in key:
+    with open(x+'.json', 'w') as f:
+        print(json.dumps(client.venues.search(params={'ll':'37.9485,-91.7715','query':x,'radius':'1000'}),sort_keys=True, indent=4, separators=(',', ': ')),file=f)
+import subprocess
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+os.chdir(dir_path)
+subprocess.call('python RestaurantExtractor.py',shell=True)
