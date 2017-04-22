5c929d9d681e709183d5f3fa95a5490c11ddf6d8
Mohammed Jasam <mnqnd@mst.edu>
2017-04-21 23:23:11 -0500

    Adjust the granularity level and also the radius of results!

--- Python-Script/Foursquare/RestaurantExtractor.py
+++ Python-Script/Foursquare/RestaurantExtractor.py
@@ -1,24 +1,23 @@
 import json
 import os
+
+## Path Navigation!
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path+'\\DataSets\\')
-key=['chicken','steak','pizza','Restaurant']
-# key=['categories']
 l=[]
 names=[]
 
-for x in key:
-    with open(x+'.json') as data_file:
-        data = json.load(data_file)
-        
-    data = dict(data)
+with open('Restaurants.json') as data_file:
+    data = json.load(data_file)
 
+    data = dict(data)
+    print("These are the Restaurants in your location:")
     for i in range(100):
         try:
             x= data['venues'][i]['name']
             if x not in names:
                 names.append(data['venues'][i]['name'])
-                print(x)
+                print(str(i+1) + ". " + x)
             else:
                 pass
 
--- Python-Script/Foursquare/localRecommender.py
+++ Python-Script/Foursquare/localRecommender.py
@@ -1,21 +1,26 @@
-import foursquare
-from foursquare import Foursquare
+import os
 import json
 import subprocess
+import foursquare
+from foursquare import Foursquare
 
 
+ll='37.9485,-91.7715'
+
+
+### Creating a Client to Access the API
 client = foursquare.Foursquare(client_id='Q3JHJJ1TS1XOR0TK1LTF5OFJHQC4OTLHMBIK1GYOIYXORSMB', client_secret='C54VXQIUTU2PKCPZI3RJQICLYNLYKZQ40ST1T50PYN5YVHUO')
-key=['Restaurant','chicken','steak','pizza']
 
+## Accessing the app from the user!
 client.users('9650322')
-import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path+'\\DataSets\\')
 
+## Extracting the data by querying!
 with open('Restaurants.json', 'w') as f:
-    print(json.dumps(client.venues.search(params={'ll':'37.9485,-91.7715','radius':'10000','categoryId':'4d4b7105d754a06374d81259'}),sort_keys=True, indent=4, separators=(',', ': ')),file=f)
+    print(json.dumps(client.venues.trending(params={'ll':ll,'radius':'10000','categoryId':'4d4b7105d754a06374d81259'}),sort_keys=True, indent=4, separators=(',', ': ')),file=f)
 
-# Displaying Results
+### Displaying Results
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 subprocess.call('python RestaurantExtractor.py',shell=True)
